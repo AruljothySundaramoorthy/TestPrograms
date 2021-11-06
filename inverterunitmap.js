626bc2177a3d441810f3c8e74979c6c9ef06a6da
Aruljothy <aruljyothi@armaxindia.com>
2021-11-06 20:07:19 +0530

    new Set () methid implementation
     - collected the list of inverterunitmap and tested teh unique parent id
       using new Set() method

---
+++ inverterunitmap.js
@@ -0,0 +1,311 @@
+const data = [
+    {
+        "deviceparentid": "c3d48133-c9b6-4c04-ba23-17acbbe0ca32"
+    },
+    {
+        "deviceparentid": "c3d48133-c9b6-4c04-ba23-17acbbe0ca32"
+    },
+    {
+        "deviceparentid": "add570b1-4f0b-42b3-992b-7036d3ab07e8"
+    },
+    {
+        "deviceparentid": "add570b1-4f0b-42b3-992b-7036d3ab07e8"
+    },
+    {
+        "deviceparentid": "a39836d4-0534-42c1-b254-09ee1c1c3c3e"
+    },
+    {
+        "deviceparentid": "a39836d4-0534-42c1-b254-09ee1c1c3c3e"
+    },
+    {
+        "deviceparentid": "1377390d-d178-4651-89ea-f847dbaf8747"
+    },
+    {
+        "deviceparentid": "1377390d-d178-4651-89ea-f847dbaf8747"
+    },
+    {
+        "deviceparentid": "fc78cc5f-fc32-42db-9dac-159874b656dc"
+    },
+    {
+        "deviceparentid": "fc78cc5f-fc32-42db-9dac-159874b656dc"
+    },
+    {
+        "deviceparentid": "1f36bf1d-b3a9-4e93-9db4-efcdf5be337a"
+    },
+    {
+        "deviceparentid": "1f36bf1d-b3a9-4e93-9db4-efcdf5be337a"
+    },
+    {
+        "deviceparentid": "e726b609-ff33-4b7c-be38-ec8c3f593a60"
+    },
+    {
+        "deviceparentid": "e726b609-ff33-4b7c-be38-ec8c3f593a60"
+    },
+    {
+        "deviceparentid": "b46d8b38-c6b9-4cf8-b786-d71fac2a006d"
+    },
+    {
+        "deviceparentid": "b46d8b38-c6b9-4cf8-b786-d71fac2a006d"
+    },
+    {
+        "deviceparentid": "e0906cc3-d812-4a02-a8c2-232263c7eadf"
+    },
+    {
+        "deviceparentid": "e0906cc3-d812-4a02-a8c2-232263c7eadf"
+    },
+    {
+        "deviceparentid": "525860cc-33f4-4dc9-a198-9db12f12961f"
+    },
+    {
+        "deviceparentid": "525860cc-33f4-4dc9-a198-9db12f12961f"
+    },
+    {
+        "deviceparentid": "44636cfc-146b-4011-a0a0-529644c205c6"
+    },
+    {
+        "deviceparentid": "2767b9e4-1e3d-4a08-a495-b0fb5a49cb67"
+    },
+    {
+        "deviceparentid": "44636cfc-146b-4011-a0a0-529644c205c6"
+    },
+    {
+        "deviceparentid": "2767b9e4-1e3d-4a08-a495-b0fb5a49cb67"
+    },
+    {
+        "deviceparentid": "4df001e1-db9c-449a-815a-39ccf4263b6a"
+    },
+    {
+        "deviceparentid": "4df001e1-db9c-449a-815a-39ccf4263b6a"
+    },
+    {
+        "deviceparentid": "09e6875f-48db-4cfa-846e-726c1612f41f"
+    },
+    {
+        "deviceparentid": "09e6875f-48db-4cfa-846e-726c1612f41f"
+    },
+    {
+        "deviceparentid": "eb86818d-da56-4917-83c7-8270cbc1acb5"
+    },
+    {
+        "deviceparentid": "eb86818d-da56-4917-83c7-8270cbc1acb5"
+    },
+    {
+        "deviceparentid": "bbcd1a0e-e899-42dd-834d-e00682c6fd7c"
+    },
+    {
+        "deviceparentid": "bbcd1a0e-e899-42dd-834d-e00682c6fd7c"
+    },
+    {
+        "deviceparentid": "a04e69e1-c39b-43f5-8a3a-4d1ab88a2544"
+    },
+    {
+        "deviceparentid": "a04e69e1-c39b-43f5-8a3a-4d1ab88a2544"
+    },
+    {
+        "deviceparentid": "2ce96926-d63c-46f4-8046-67d1b0ef5215"
+    },
+    {
+        "deviceparentid": "2ce96926-d63c-46f4-8046-67d1b0ef5215"
+    },
+    {
+        "deviceparentid": "d2718128-39ff-4121-afe8-18083c7bac99"
+    },
+    {
+        "deviceparentid": "d2718128-39ff-4121-afe8-18083c7bac99"
+    },
+    {
+        "deviceparentid": "87075010-2507-43e0-b71f-e2b384f77892"
+    },
+    {
+        "deviceparentid": "87075010-2507-43e0-b71f-e2b384f77892"
+    },
+    {
+        "deviceparentid": "88a9ba0a-7815-44ad-a006-6587bd709999"
+    },
+    {
+        "deviceparentid": "88a9ba0a-7815-44ad-a006-6587bd709999"
+    },
+    {
+        "deviceparentid": "29572d0f-a84e-45fe-8830-b71c4137ca1c"
+    },
+    {
+        "deviceparentid": "29572d0f-a84e-45fe-8830-b71c4137ca1c"
+    },
+    {
+        "deviceparentid": "04417668-ea66-4b3c-9238-f9cf6d1b4bdf"
+    },
+    {
+        "deviceparentid": "04417668-ea66-4b3c-9238-f9cf6d1b4bdf"
+    },
+    {
+        "deviceparentid": "fb280a19-c807-4a64-8931-d2ff09bd2c16"
+    },
+    {
+        "deviceparentid": "fb280a19-c807-4a64-8931-d2ff09bd2c16"
+    },
+    {
+        "deviceparentid": "e845bb28-ac25-457d-af27-a2b1ea3157d6"
+    },
+    {
+        "deviceparentid": "e845bb28-ac25-457d-af27-a2b1ea3157d6"
+    },
+    {
+        "deviceparentid": "b4d51a51-c8ea-4be9-8649-affd6522d5a6"
+    },
+    {
+        "deviceparentid": "b4d51a51-c8ea-4be9-8649-affd6522d5a6"
+    },
+    {
+        "deviceparentid": "fc95e42c-ffa4-4847-8121-d6fcf13ae5a6"
+    },
+    {
+        "deviceparentid": "fc95e42c-ffa4-4847-8121-d6fcf13ae5a6"
+    },
+    {
+        "deviceparentid": "8217b84e-d5e9-4494-9686-81d504567bf2"
+    },
+    {
+        "deviceparentid": "d3de159e-ef04-4c16-8287-793599e5c437"
+    },
+    {
+        "deviceparentid": "8217b84e-d5e9-4494-9686-81d504567bf2"
+    },
+    {
+        "deviceparentid": "d3de159e-ef04-4c16-8287-793599e5c437"
+    },
+    {
+        "deviceparentid": "b4123ee1-881d-45c4-8f0c-5bc01e975a05"
+    },
+    {
+        "deviceparentid": "b4123ee1-881d-45c4-8f0c-5bc01e975a05"
+    },
+    {
+        "deviceparentid": "cf745a76-e8e6-4bb8-bca6-b968da2a5533"
+    },
+    {
+        "deviceparentid": "cf745a76-e8e6-4bb8-bca6-b968da2a5533"
+    },
+    {
+        "deviceparentid": "d4ef753f-87a3-44ca-a2c7-8ee49e27949f"
+    },
+    {
+        "deviceparentid": "d4ef753f-87a3-44ca-a2c7-8ee49e27949f"
+    },
+    {
+        "deviceparentid": "b68b39a7-8f79-43e0-841f-17cdefac0e35"
+    },
+    {
+        "deviceparentid": "b68b39a7-8f79-43e0-841f-17cdefac0e35"
+    },
+    {
+        "deviceparentid": "3dae9d96-1b6e-4843-b430-2475326a17a5"
+    },
+    {
+        "deviceparentid": "ff3419d3-f1cf-4050-80d9-9d56fa8abeb3"
+    },
+    {
+        "deviceparentid": "3dae9d96-1b6e-4843-b430-2475326a17a5"
+    },
+    {
+        "deviceparentid": "ff3419d3-f1cf-4050-80d9-9d56fa8abeb3"
+    },
+    {
+        "deviceparentid": "bd78c434-aca3-4193-b990-a1e179a0d0d3"
+    },
+    {
+        "deviceparentid": "bd78c434-aca3-4193-b990-a1e179a0d0d3"
+    },
+    {
+        "deviceparentid": "ca2081bd-6f34-4af7-adf9-ffaad895ba32"
+    },
+    {
+        "deviceparentid": "ca2081bd-6f34-4af7-adf9-ffaad895ba32"
+    },
+    {
+        "deviceparentid": "3ff9a5d9-64d3-42f0-a5fb-54a94847f8d6"
+    },
+    {
+        "deviceparentid": "3ff9a5d9-64d3-42f0-a5fb-54a94847f8d6"
+    },
+    {
+        "deviceparentid": "dd03ad6b-2152-4dfe-86b5-9a51e42726be"
+    },
+    {
+        "deviceparentid": "dd03ad6b-2152-4dfe-86b5-9a51e42726be"
+    },
+    {
+        "deviceparentid": "1796f4f3-262c-46fc-8195-c33a78bb63b7"
+    },
+    {
+        "deviceparentid": "1796f4f3-262c-46fc-8195-c33a78bb63b7"
+    },
+    {
+        "deviceparentid": "0bef8ba2-ac7c-42d8-8c97-d40846e069f5"
+    },
+    {
+        "deviceparentid": "0bef8ba2-ac7c-42d8-8c97-d40846e069f5"
+    },
+    {
+        "deviceparentid": "ce9d1f10-938e-4227-9462-bcbdd2bc9266"
+    },
+    {
+        "deviceparentid": "ce9d1f10-938e-4227-9462-bcbdd2bc9266"
+    },
+    {
+        "deviceparentid": "ca1569db-3697-415c-8539-02167a9f26d5"
+    },
+    {
+        "deviceparentid": "ca1569db-3697-415c-8539-02167a9f26d5"
+    },
+    {
+        "deviceparentid": "abf76644-f70f-475f-b1a0-91a939e86a98"
+    },
+    {
+        "deviceparentid": "5a942b96-4242-4096-a8f1-c9eeeb11e6c7"
+    },
+    {
+        "deviceparentid": "5a942b96-4242-4096-a8f1-c9eeeb11e6c7"
+    },
+    {
+        "deviceparentid": "abf76644-f70f-475f-b1a0-91a939e86a98"
+    },
+    {
+        "deviceparentid": "0497642f-35f5-47ac-ac56-cdfbdcf31e1e"
+    },
+    {
+        "deviceparentid": "0497642f-35f5-47ac-ac56-cdfbdcf31e1e"
+    },
+    {
+        "deviceparentid": "90494c0c-c31a-4a6d-a922-10b8328bc6ed"
+    },
+    {
+        "deviceparentid": "90494c0c-c31a-4a6d-a922-10b8328bc6ed"
+    },
+    {
+        "deviceparentid": "223a366a-0b8d-45c1-b03d-535ea3394b5f"
+    },
+    {
+        "deviceparentid": "223a366a-0b8d-45c1-b03d-535ea3394b5f"
+    },
+    {
+        "deviceparentid": "2c0d0271-a584-4d8e-add5-17ef4d10a16a"
+    },
+    {
+        "deviceparentid": "2c0d0271-a584-4d8e-add5-17ef4d10a16a"
+    },
+    {
+        "deviceparentid": "9084ed2b-62a4-46b7-bb3b-3e45f34c3c90"
+    },
+    {
+        "deviceparentid": "9084ed2b-62a4-46b7-bb3b-3e45f34c3c90"
+    },
+    {
+        "deviceparentid": "7f713a0f-ea35-4e09-a444-caed00e34ee9"
+    },
+    {
+        "deviceparentid": "7f713a0f-ea35-4e09-a444-caed00e34ee9"
+    }
+];
+const d = data.map((x) => x.deviceparentid)
+
+console.log(data);
