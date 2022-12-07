3bf74d6abb04e1a4e2d3181c6203ea98a71beb6e
Aruljothy Sundaramoorthy <aruljothy.s@fleximc.com>
2022-12-07 11:53:19 +0530

    cryptojs

--- server5.js
+++ server5.js
@@ -814,6 +814,37 @@ try {
       },
     };
   };
+  const createOuterBorder = (worksheet, start = {row: 1, col: 1}, end = {row: 1, col: 1}, borderWidth = 'medium') => {
+
+    const borderStyle = {
+        style: borderWidth
+    };
+    for (let i = start.row; i <= end.row; i++) {
+        const leftBorderCell = worksheet.getCell(i, start.col);
+        const rightBorderCell = worksheet.getCell(i, end.col);
+        leftBorderCell.border = {
+            ...leftBorderCell.border,
+            left: borderStyle
+        };
+        rightBorderCell.border = {
+            ...rightBorderCell.border,
+            right: borderStyle
+        };
+    }
+
+    for (let i = start.col; i <= end.col; i++) {
+        const topBorderCell = worksheet.getCell(start.row, i);
+        const bottomBorderCell = worksheet.getCell(end.row, i);
+        topBorderCell.border = {
+            ...topBorderCell.border,
+            top: borderStyle
+        };
+        bottomBorderCell.border = {
+            ...bottomBorderCell.border,
+            bottom: borderStyle
+        };
+    }
+};
   const generateecel = (req, res) => {
     const workbook = new ExcelJS.Workbook();
 
@@ -824,124 +855,147 @@ try {
     workbook.lastPrinted = new Date(2016, 9, 27);
 
     const sheet = workbook.addWorksheet("My Users"); // New Worksheet
-    /*TITLE*/
-    sheet.getCell("A1").value = "Report Name";
-    sheet.getCell("A1").style = addBorder();
-    sheet.getCell("A2").value = "Date of Report Start Time";
-    sheet.getCell("A2").style = addBorder();
-    sheet.getCell("A3").value = "Date of Report End Time";
-    sheet.getCell("A3").style = addBorder();
-    sheet.getCell("B1").value = "Daily generation report";
-    sheet.getCell("B1").style = addBorder();
-    sheet.getCell("B2").value = "27/10/2022 00:00AM";
-    sheet.getCell("B2").style = addBorder();
-    sheet.getCell("B3").value = "28/10/2022 00:00AM";
-    sheet.getCell("B3").style = addBorder();
+    // /*TITLE*/
+    // sheet.getCell("A1").value = "Report Name";
+    // sheet.getCell("A1").style = addBorder();
+    // sheet.getCell("A2").value = "Date of Report Start Time";
+    // sheet.getCell("A2").style = addBorder();
+    // sheet.getCell("A3").value = "Date of Report End Time";
+    // sheet.getCell("A3").style = addBorder();
+    // sheet.getCell("B1").value = "Daily generation report";
+    // sheet.getCell("B1").style = addBorder();
+    // sheet.getCell("B2").value = "27/10/2022 00:00AM";
+    // sheet.getCell("B2").style = addBorder();
+    // sheet.getCell("B3").value = "28/10/2022 00:00AM";
+    // sheet.getCell("B3").style = addBorder();
 
-    sheet.mergeCells("C1", "I3");
-    sheet.getCell("C1").value = "Azure Power";
-    sheet.getCell("C1").style = {
-      font: { bold: true, color: "black", size: 24 },
-      alignment: { horizontal: "center", vertical: "middle" },
-      fill: {
-        type: "pattern",
-        pattern: "solid",
-        fgColor: {
-          argb: "f8cbad",
-        },
-      },
-    };
-
-    const cellRange = numberToAlphabets((Object.keys(devices).length + 1) * 3);
-
-    sheet.mergeCells("A4", `${cellRange}4`);
-    sheet.getCell("A4").value = "Daily Metring";
-    sheet.getCell("A4").style = {
-      font: { bold: true, color: "black", size: 14 },
-      alignment: { horizontal: "center", vertical: "middle" },
-      fill: {
-        type: "pattern",
-        pattern: "solid",
-        fgColor: {
-          argb: "c6e0b4",
-        },
-      },
-    };
-
-    sheet.mergeCells("A5", "A6");
-    sheet.getCell("A5").value = "Date";
-    sheet.getCell(`"A5"`).style = {
-      font: { bold: true },
-      alignment: { horizontal: "center", vertical: "middle" },
-    };
-    let prentRowNumber = 2;
-    Object.keys(devices).forEach((deviceid, index) => {
-      const startcell = `${numberToAlphabets(prentRowNumber)}5`;
-      const endcell = `${numberToAlphabets(prentRowNumber + 2)}5`;
-      sheet.mergeCells(`${startcell}:${endcell}`);
-      sheet.getCell(`${startcell}`).value = devices[deviceid];
-      sheet.getCell(`${startcell}`).style = {
-        font: { bold: true },
-        alignment: { horizontal: "center", vertical: "middle" },
-      };
-
-      for (let i = 0; i <= prentRowNumber + 2; i++) {
-        if (i == 0) {
-          sheet.getCell(`${numberToAlphabets(prentRowNumber + i)}6`).value =
-            "Export";
-        }
-        if (i == 1) {
-          sheet.getCell(`${numberToAlphabets(prentRowNumber + i)}6`).value =
-            "Import";
-        }
-        if (i == 2) {
-          sheet.getCell(`${numberToAlphabets(prentRowNumber + i)}6`).value =
-            "Net Export";
-        }
-        sheet.getCell(`${numberToAlphabets(prentRowNumber + i)}6`).style = {
-          font: { bold: true },
-          alignment: { horizontal: "center", vertical: "middle" },
-        };
-      }
-
-      prentRowNumber = prentRowNumber + 3;
-    });
-
-    autoWidth(sheet);
-
-    // sheet.getRow(9).values = deviceRowName;
-
-    // sheet.columns = deviceColumns;
-    const rowInfo = Object.keys(devicedataval[0]);
-    // const parmeterInfo = {
-    //   planttimestamp: "Planttimestamp",
-    //   kwhExportTotal: "Import",
-    //   kwhImportTotal: "Export",
-    //   kwhNetExportTotal: "Net Export",
+    // sheet.mergeCells("C1", "I3");
+    // sheet.getCell("C1").value = "Azure Power";
+    // sheet.getCell("C1").style = {
+    //   font: { bold: true, color: "black", size: 24 },
+    //   alignment: { horizontal: "center", vertical: "middle" },
+    //   fill: {
+    //     type: "pattern",
+    //     pattern: "solid",
+    //     fgColor: {
+    //       argb: "f8cbad",
+    //     },
+    //   },
     // };
 
-    arrData = devicedataval;
-    // /* Now we use the keys we defined earlier to insert your data by iterating through arrData and calling worksheet.addRow()
-    //  */
-    arrData.forEach(function (item, index) {
-      let temprowdata = [];
-      rowInfo.map((row) => {
-        let splitinder = row=='planttimestamp'?'planttimestamp' :  row.split('_')[1]
-        if (validatekeyFromObject(item,row)) {
-          // Object.assign(temprowdata, { [parmeterInfo[splitinder]]: item[row] });
-          temprowdata.push(item[row])
-        } else {
-          // Object.assign(temprowdata, { [parmeterInfo[splitinder]]: '-' });
-          temprowdata.push('-')
-        }
-        // console.log(row,item)
-      });
-      sheet.addRow(temprowdata);
-    });
-    
-      // sheet.addRow([1,2,1233123,123,12,312,312,31,23123,123123]);
-    autoWidth(sheet);
+    // createOuterBorder(sheet,{row:sheet.getCell("C1").row,col:sheet.getCell("C1").col},{row:sheet.getCell("I1").row,col:sheet.getCell("I1").col},'thin')
+    // const cellRange = numberToAlphabets((Object.keys(devices).length + 1) * 3);
 
+   
+    // sheet.mergeCells("A4", `${cellRange}4`);
+    // sheet.getCell("A4").value = "Daily Metring";
+    // sheet.getCell("A4").style = {
+    //   font: { bold: true, color: "black", size: 14 },
+    //   alignment: { horizontal: "center", vertical: "middle" },
+    //   fill: {
+    //     type: "pattern",
+    //     pattern: "solid",
+    //     fgColor: {
+    //       argb: "c6e0b4",
+    //     },
+    //   },
+    // };
+
+    // createOuterBorder(sheet,{row:sheet.getCell("A4").row,col:sheet.getCell("A4").col},{row:sheet.getCell(`${cellRange}4`).row,col:sheet.getCell(`${cellRange}4`).col})
+    // sheet.mergeCells("A5", "A6");
+    // sheet.getCell("A5").value = "Date";
+    // sheet.getCell(`"A5"`).style = {
+    //   font: { bold: true },
+    //   alignment: { horizontal: "center", vertical: "middle" },
+    // };
+
+    // createOuterBorder(sheet,{row:sheet.getCell('A5').row,col:sheet.getCell('A5').col},{row:sheet.getCell('A6').row,col:sheet.getCell('A6').col})
+   
+    
+
+    // let prentRowNumber = 2;
+    // Object.keys(devices).forEach((deviceid, index) => {
+    //   const startcell = `${numberToAlphabets(prentRowNumber)}5`;
+    //   const endcell = `${numberToAlphabets(prentRowNumber + 2)}5`;
+    //   sheet.mergeCells(`${startcell}:${endcell}`);
+    //   sheet.getCell(`${startcell}`).value = devices[deviceid];
+    //   sheet.getCell(`${startcell}`).style = {
+    //     font: { bold: true },
+    //     alignment: { horizontal: "center", vertical: "middle" },
+    //   };
+
+    //   createOuterBorder(sheet,{row:sheet.getCell(startcell).row,col:sheet.getCell(startcell).col},{row:sheet.getCell(endcell).row,col:sheet.getCell(endcell).col})
+   
+    //   for (let i = 0; i <= prentRowNumber + 2; i++) {
+    //     const parentcolumnnumber = `${numberToAlphabets(prentRowNumber + i)}6`;
+    //     if (i == 0) {
+    //       sheet.getCell(parentcolumnnumber).value =
+    //         "Export";
+    //     }
+    //     if (i == 1) {
+    //       sheet.getCell(parentcolumnnumber).value =
+    //         "Import";
+    //     }
+    //     if (i == 2) {
+    //       sheet.getCell(parentcolumnnumber).value =
+    //         "Net Export";
+    //     }
+    //     sheet.getCell(parentcolumnnumber).style = {
+    //       font: { bold: true },
+    //       alignment: { horizontal: "center", vertical: "middle" },
+    //     };
+    //     createOuterBorder(sheet,{row:sheet.getCell(parentcolumnnumber).row,col:sheet.getCell(parentcolumnnumber).col},{row:sheet.getCell(parentcolumnnumber).row,col:sheet.getCell(parentcolumnnumber).col})
+    //   }
+
+    //   prentRowNumber = prentRowNumber + 3;
+    // });
+
+    // autoWidth(sheet);
+
+    // // sheet.getRow(9).values = deviceRowName;
+
+    // // sheet.columns = deviceColumns;
+    // const rowInfo = Object.keys(devicedataval[0]);
+    // // const parmeterInfo = {
+    // //   planttimestamp: "Planttimestamp",
+    // //   kwhExportTotal: "Import",
+    // //   kwhImportTotal: "Export",
+    // //   kwhNetExportTotal: "Net Export",
+    // // };
+
+    // arrData = devicedataval;
+    // // /* Now we use the keys we defined earlier to insert your data by iterating through arrData and calling worksheet.addRow()
+    // //  */
+    // arrData.forEach(function (item, index) {
+    //   let temprowdata = [];
+    //   rowInfo.map((row) => {
+    //     let splitinder = row=='planttimestamp'?'planttimestamp' :  row.split('_')[1]
+    //     if (validatekeyFromObject(item,row)) {
+    //       // Object.assign(temprowdata, { [parmeterInfo[splitinder]]: item[row] });
+    //       temprowdata.push(item[row])
+    //     } else {
+    //       // Object.assign(temprowdata, { [parmeterInfo[splitinder]]: '-' });
+    //       temprowdata.push('-')
+    //     }
+    //     // console.log(row,item)
+    //   });
+    //   sheet.addRow(temprowdata);
+    //   sheet.lastRow._cells.forEach((cell)=>{cell.style={border: {
+    //     top: { style: "thin" },
+    //     bottom: { style: "thin" },
+    //     left: { style: "thin" },
+    //     right: { style: "thin" },
+    //   }}})
+    // });
+    
+    //   // sheet.addRow([1,2,1233123,123,12,312,312,31,23123,123123]);
+    // autoWidth(sheet);
+
+    sheet.columns = [
+      { header: 'Id', key: 'id', width: 10 },
+      { header: 'Name', key: 'name', width: 32 },
+      { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
+    ];
     res.setHeader(
       "Content-Type",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
