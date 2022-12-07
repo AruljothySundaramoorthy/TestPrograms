24459639a082714656bdb50e22c1613d784e5d89
Aruljothy Sundaramoorthy <aruljothy.s@fleximc.com>
2022-12-07 12:17:05 +0530

    helper

--- crypto.js
+++ crypto.js
@@ -1,32 +1,29 @@
-
-// npm i crypto-js
+/**
+ *
+ * npm i crypto-js
+ * */
 
 var CryptoJS = require("crypto-js");
-const secretKey = 'ARULJOTHY'
+const secretKey = "ARULJOTHY";
 function genRandonString(length) {
-   var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-   var charLength = chars.length;
-   var result = '';
-   for ( var i = 0; i < length; i++ ) {
-      result += chars.charAt(Math.floor(Math.random() * charLength));
-   }
-   return result;
+  var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var charLength = chars.length;
+  var result = "";
+  for (var i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * charLength));
+  }
+  return result;
 }
- 
-const generatedString=genRandonString(256)
-console.log('Original String ',generatedString);
 
-
+const generatedString = genRandonString(256);
+console.log("Original String ", generatedString);
 
 // Encrypt
 var ciphertext = CryptoJS.AES.encrypt(generatedString, secretKey).toString();
-console.log('Encrypted ',ciphertext)
+console.log("Encrypted ", ciphertext);
 
 // Decrypt
-var bytes  = CryptoJS.AES.decrypt(ciphertext,secretKey);
+var bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
 var originalText = bytes.toString(CryptoJS.enc.Utf8);
 
-console.log('Decrypted ',originalText); // 'my message'
-
- 
- 
+console.log("Decrypted ", originalText); // 'my message'
